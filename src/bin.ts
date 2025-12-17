9d5e147e30f9a2b3a1898af12edf5363e48338d5
Antoine Foures <contact@afoures.com>
2025-12-17 11:12:39 +0100

    add list command

--- src/bin.ts
+++ src/bin.ts
@@ -6,6 +6,7 @@ import { record_change } from "./lib/commands/record.js";
 import { generate_release } from "./lib/commands/generate-release.js";
 import { tag_release } from "./lib/commands/tag-release.js";
 import { init } from "./lib/commands/init.js";
+import { list } from "./lib/commands/list.js";
 
 const run = create_cli({
   name: "auto-release",
@@ -13,6 +14,7 @@ const run = create_cli({
   commands: {
     init,
     check,
+    list,
     "record-change": record_change,
     "generate-release": generate_release,
     "tag-release": tag_release,
